c0c4d7bf224cd1fd06c988b741b47beb2f5ded58
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2023-08-21 09:24:36 -0400

    Add files via upload

---
+++ ScratchWebsite/backend/frontend/static/animate.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const list_item = document.querySelector(".list-item");
+    list_item.classList.add("animate-fade-in");
+    const title = document.querySelector(".offer");
+    title.classList.add("animate-fade-in");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.querySelector(".bouncing-text");
+    textElement.classList.add("bouncing-text");
+});
