103fbfb9bff17dbea5d9f4c85e31bf7d85aee3df
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2023-09-02 21:27:58 -0400

    Update views.py

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .form import ReviewForm
+
 
 def home(request):
     return render(
         request,
-        'home.html')
+        'home.html'
+        )
 def menu(request):
     return render(
         request,
-        'menu.html')
+        'menu.html'
+        )
+def about(request):
+    return render(
+        request,
+        'about.html'
+        )
+def sponsers(request):
+    return render(
+        request,
+        'sponsers.html'
+        )
+def careers(request):
+    return render(
+        request,
+        'careers.html'
+        )
+
+
+def reviewForm(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Form submitted successfully")
+        else:
+            return HttpResponse("Form is not valid")
+    else:
+        form = ReviewForm()
+        return render(request, 'contact.html', {'form': form})
